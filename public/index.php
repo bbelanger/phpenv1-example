73bbb3467c1e1af87c4ebfc22ccd498d5c9dd368
Brian Belanger <brian.belanger@outlook.com>
2018-06-08 04:46:04 +0000

    First commit

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Medoo\Medoo;
+
+echo "Hello World";
+
+$database = new Medoo([
+    'database_type' => 'sqlite',
+    'database_file' => '../storage/database.db'
+]);
+
+dump($database);
